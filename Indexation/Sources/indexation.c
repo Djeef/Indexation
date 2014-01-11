f8228fa6e04662232ee14edbe9db724ba7046a80
Jean-François Erdelyi <jean-francois.erdelyi@univ-tlse3.fr>
2014-01-11 18:39:14 +0100

    Indexation : add comment Doxygen by Aurelien
    Signed-off-by: Jean-François Erdelyi <jean-francois.erdelyi@univ-tlse3.fr>

--- Indexation/Sources/indexation.c
+++ Indexation/Sources/indexation.c
@@ -7,6 +7,125 @@
 #include "Sound/wavFile.h"
 #include "Picture/openPicture.h"
 
+/**
+ * \mainpage DOCUMENTATION: Indexation d'une base de données - Indexing of data base
+ *
+ * \section section_cadre I - Cadre du projet - Context of the project
+ * 
+ * Ce projet s'inscrit dans le cadre de la formation L3 SMI 2013-2014 dispensée par l'Université Paul Sabatier de Toulouse.
+ * 
+ * Ce projet a été réalisé par: ERDELYI Jean-François, VEILLARD Aurélien et ZERBIB Yoanna.
+ * 
+ * __
+ * 
+ * This project takes place in L3 SMI formation delivered by Paul Sabatier University of Toulouse.
+ * 
+ * This project was directed by ERDELYI Jean-François, VEILLARD Aurélien et ZERBIB Yoanna.
+ * ___
+ * 
+ * \section section_introduction II - Présentation - Overview
+ *
+ * Le but de ce projet était de créer un programme capable de traiter trois types de fichiers: 
+ * 
+ * 	- Les fichiers audio de types wave (.wav);
+ * 
+ * 	- Les fichiers images de types bmp et jpeg au format texte (.txt);
+ * 
+ * 	- Les fichiers textes de types xml.
+ * 
+ * L'indexation représente la première partie du projet à laquelle s'ajoutera un moteur de recherche.
+ * 
+ * __
+ * 
+ * The aim of this project was to create un program which is be able to process three types of files:
+ * 
+ * 	- Wave sound files (.wav);
+ * 
+ * 	- Picture files (jpeg and bmp) formatted as text files;
+ * 
+ * 	- Text files in format .xml.
+ * 
+ * The indexing part is the first part of the project that will add a search engine.
+ * ___
+ *
+ * \section section_installation III - Utilisation - Using
+ * 
+ * \subsection subsection_prerequis 1 - Pré-requis - Prerequisites
+ * 
+ * Quelques points sont à respecter avant de lancer l'indexation:
+ * 
+ * 	- Les fichiers à indexer doivent être dans les formats suivants: .wav, .xml, .txt (correspondant à un .jpeg ou bmp);
+ * 
+ * 	- L'ensemble de ces fichiers doivent être placé dans un seul et même fichier nommé "Data";
+ * 
+ * 	- Le fichier "config.conf" doit absolument être présent et complet.
+ * 
+ * __
+ * 
+ * Some points must be respected before processing indexing:
+ * 
+ * 	- The index files must be in following formats: .wav, .xml, .txt (corresponding to a jpeg or a bmp file);
+ * 
+ * 	- All files should be place in only one folder named "Data";
+ * 
+ * 	- The "config.conf" file should be absolutely present and complete.
+ * 
+ * ___
+ * 
+ * \subsection subsection_installation 2 - Installation et lancement - Installation and running
+ * 
+ * Afin de procéder à l'indexation de la base de données, il faut que l'exécutable "Indexation", que le fichier "config.conf" et que le dossier "Data" soit au même niveau ou dans le même dossier.
+ * 
+ * Pour l'exécuter, il suffit de double cliquer dessus ou bien d'écrire ceci dans la fenêtre de commande:
+ * 
+ * \code
+ * $ ./Indexation
+ * \endcode
+ * 
+ * __
+ * 
+ * To perform indexing of the database, it's necessary that the executable "Indexation", the "config.conf" file and the "Data" folder is at the same level or in the same folder.
+ * 
+ * To run it, just double click on it or write it in the command window:
+ *  
+ * \code
+ * $ ./Indexation
+ * \endcode
+ * ___
+ * 
+ * \subsection subsection_sortie 3 - Données de sortie - Output datas
+ * 
+ * Après exécution du programme, un dossier "Bases" a été créer automatiquement contenant les fichiers suivants:
+ * 
+ * 	- liste_base_audio.base : contenant les identifiants uniques et les fichiers associés (idem pour image et texte);
+ * 
+ * 	- base_descripteur_audio.base : contenant l'ensemble des descripteurs des fichiers traités (idem pour image et texte).
+ * 
+ * Ces fichiers pourront être ensuite utilisés pour le moteur de recherche.
+ * 
+ * __
+ * 
+ * After running, "Bases" folder was automatically created containing the following files:
+ * 
+ * 	- liste_base_audio.base: containing unique identifiers and associated files (same for image and text);
+ * 
+ * 	- base_descripteur_audio.base: containing all descriptors processed files (same for image and text).
+ * 
+ * Then, these files can be used for search engine.
+ * 
+ * ___
+ * 
+ * \section copyright Copyright and License
+ * 
+ * Ce projet est sous licence <a href="http://www.gnu.org/licenses/gpl-2.0.html">GNU GENERAL PUBLIC LICENSE v2</a>.
+ * 
+ * __
+ * 
+ * This project is under <a href="http://www.gnu.org/licenses/gpl-2.0.html">GNU GENERAL PUBLIC LICENSE v2</a>.
+ * 
+ */
+
+
 int main(int argc, char *argv[]) {
 	struct dirent *currentElement;
 	DIR *data;
